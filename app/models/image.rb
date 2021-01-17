cfda82a6e9726f5363b946d63194c55264616324
Jerry Hall <hall.jerry9@gmail.com>
2021-01-16 21:44:02 -0800

    misc. cleanup

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,8 +2,8 @@ class Image < ApplicationRecord
     has_one_attached :image_blob, dependent: :destroy
     after_validation :is_image?
 
+    # Validate image is attached
     def is_image?
         errors.add(:base, 'Please upload your image.') if !self.image_blob.attached?
     end
-    # validates :image_blob, presence: true
 end
