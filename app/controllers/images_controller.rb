e3614b6b0dad1632c0e14ce6f961c0d16ce59150
Jerry Hall <hall.jerry9@gmail.com>
2021-01-16 22:25:00 -0800

    misc. cleanup

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -56,7 +56,7 @@ class ImagesController < ApplicationController
   def destroy
     @image.destroy
     respond_to do |format|
-      format.html { redirect_to images_url, notice: 'Image was successfully destroyed.' }
+      format.html { redirect_to images_url, notice: 'Image was successfully deleted.' }
       format.json { head :no_content }
     end
   end
@@ -70,11 +70,5 @@ class ImagesController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def image_params
       params.fetch(:image, {}).permit(:image_blob)
-    end
-
-    def validate_image_blob
-      puts("hello")
-      # errors.add(:image, "test")
-      return params.has_key?(:image_blob)
     end
 end
